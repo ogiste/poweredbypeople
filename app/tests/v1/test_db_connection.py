58cc39706607e86847d3376c91de0a557a95aa2c
ogiste <alfred.opon@strathmore.edu>
2020-02-06 18:06:38 +0300

    Feature #171123604: Basic User Resource
    - Add db config for tables
    - Add the user model
    - Add the user view
    - Add helpers for the user model and view
    - Implement register endpoint
    - Implement login endpoint
    - Implement profile endpoint
    
    [ Finishes #171123604]

---
+++ app/tests/v1/test_db_connection.py
@@ -0,0 +1,53 @@
+"""
+Testcase for the database connection success
+"""
+import os
+import unittest
+
+from app.db_config import connect, create_tables, drop_tables, \
+    get_create_queries, get_drop_queries
+
+
+class TestIncidents(unittest.TestCase):
+    """
+    Class defining tests for verifying database connection
+    """
+
+    def setUp(self):
+        """
+        Method that is called to set the default connection and messages for
+        for testing.
+        """
+        db_name = os.getenv("DB_NAME", default="poweredbypeople_test")
+        self.conn = connect(db_name)
+        self.create_tables_message = "Tables created"
+        self.drop_tables_message = "Tables dropped"
+
+    def test_create_tables(self):
+        """
+        Method that is called to test the created tables
+        """
+        create_success = create_tables(self.conn)
+        self.assertTrue(create_success)
+
+    def test_drop_tables(self):
+        """
+        Method that is called to test the dropped tables
+        """
+        drop_success = drop_tables(self.conn)
+        self.assertTrue(drop_success)
+
+    def test_get_create_queries(self):
+        """
+        Method to test the get_create_queries method of the db config
+        """
+        self.assertIsInstance(get_create_queries(), list)
+
+    def test_get_drop_queries(self):
+        """
+        Method to test the get_drop_queries method of the db config
+        """
+        self.assertIsInstance(get_drop_queries(), list)
+
+    def tearDown(self):
+        self.conn.close()
---
+++ app/tests/v1/test_validator_methods.py
@@ -0,0 +1,81 @@
+"""
+Test cases for user classes
+"""
+import unittest
+
+from app import create_app
+from app.api.helpers.auth_validation import validate_auth_post_input
+from app.api.helpers.errors import Validation
+
+
+class TestValidatorMethods(unittest.TestCase):
+    """
+    This class represents the user test cases that test validation of a user
+    """
+
+    def setUp(self):
+        """
+        This method will be called before all tests and sets up the dummy data
+        """
+
+        self.app = create_app("testing")
+        self.user = {
+
+            "fname": "Jacob",
+            "lname": "Tutu",
+            "othername": "Damon",
+            "email": "jtutu@gmail.com",
+            "phone": "+254705093322",
+            "username": "jtutu",
+            "password": "password1",
+            "role": "vendor"
+        }
+
+        self.usercredentials = {
+            "username": "masu",
+            "password": "ssdfa12"
+        }
+
+        self.bad_usercredentials = {
+            "username": "masu",
+            "password": "ssd2"
+        }
+
+        self.validator = Validation()
+
+    def test_all_user_property_validators(self):
+        """
+        Method tests the validator methods used in testing user properties
+        """
+        self.assertTrue(self.validator.is_string('string'))
+        self.assertTrue(self.validator.is_in_limit('string'))
+        self.assertFalse(self.validator.is_in_limit('s'))
+        self.assertEqual(self.validator.remove_whitespace(12), None)
+        self.assertEqual(self.validator.remove_lr_whitespace(" dsad "), "dsad")
+        self.assertTrue(self.validator.is_valid_email(self.user["email"]))
+        self.assertTrue(self.validator.is_valid_phone(self.user["phone"]))
+        self.assertTrue(self.validator.is_valid_password(
+            self.user["password"])
+        )
+
+    def test_all_auth_property_validators(self):
+        """
+        Method tests the validator methods used in testing
+        authentication properties
+        """
+        with self.app.test_request_context('/api/v2/auth'):
+            self.assertTrue(
+                validate_auth_post_input(
+                    self.validator, self.usercredentials
+                )
+            )
+            res = validate_auth_post_input(
+                self.validator, self.bad_usercredentials
+            )
+            self.assertEqual(
+                res.status_code,
+                400
+            )
+
+    def tearDown(self):
+        pass
